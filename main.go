590ebd9ff0f09a50e36ebc44fefcb86f7e366fc2
nhatthm <thmnhat@gmail.com>
2022-03-06 22:04:49 +0100

    Use a different user agent

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ const (
 	// defaultTimeout is the default timeout for requesting an url.
 	defaultTimeout = 30 * time.Second
 	// defaultUserAgent is the default user agent to disguise.
-	defaultUserAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56`
+	defaultUserAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36`
 
 	// Limitation for number of workers to avoid resource saturation.
 	maxNumWorkers = 24
